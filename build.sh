ea06dec5318e4fe25a968be603f65c4f8deeed68
LesProfBaza Deploy <deploy@lesprofbaza.local>
2026-07-03 01:43:27 +0300

    Деплой-фикс: прямая отдача статики (CompressedStaticFilesStorage вместо Manifest) + init_site некритичен в build.sh, чтобы сборка не замерзала на старой версии и правки CSS доезжали

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py init_site
+python manage.py init_site || echo 'init_site failed (non-fatal), continuing'
